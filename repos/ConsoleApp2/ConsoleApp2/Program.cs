0b5b59905df56cf42b925da19d3ef3ecaea8d888
farrah24 <msfarrahd@gmail.com>
2021-11-13 10:03:07 -0600

    completed sugars exercise

---
+++ repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MethodOverloading
+
+{
+    class Program
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue);
+        {
+            var sum = num1 + num2;
+            
+            if(isTrue)
+            {
+            if(sum == 1)
+            { 
+             return $"{sum} dollar";
+            }
+            else
+            {
+            return $"{sum} dollars";
+            }
+        }
+        else    
+        {    
+            
+            }
+         }   
+            
+            static void Main(string[] args)
+            {
+                Add(6, 19);
+                Add(3.2, 67.5m);
+            }
+
+    }
+}
---
+++ repos/SYNTAX AND SYNTAX SUGAR/SYNTAX AND SYNTAX SUGAR/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SYNTAX_AND_SYNTAX_SUGAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? answer + "is less than nine" : 
+                answer + "greater than or equal to nine";
+                        
+            Console.WriteLine(response);
+            System.Diagnostics.Debug.WriteLine(response);
+            
+        }
+    }
+}
